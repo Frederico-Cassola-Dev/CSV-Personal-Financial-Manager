82f385c9d3ebdb52ca840d965380449b41f137df
Frederico Cassola <cfcassola@gmail.com>
2023-07-01 19:36:02 +0200

    Add base config sass

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
+import "./styles.scss";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
