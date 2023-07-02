32dd557d94f7418fc713a34328649e24c9d163ce
Frederico Cassola <cfcassola@gmail.com>
2023-07-02 16:57:34 +0200

    Add just taillwind to the uploads list table

--- frontend/src/pages/Uploads.jsx
+++ frontend/src/pages/Uploads.jsx
@@ -1,14 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 
-export default function BasicTable() {
+export default function Uploads() {
   const [uploadsData, setUploadsData] = useState([]);
 
   useEffect(() => {
@@ -18,36 +11,43 @@ export default function BasicTable() {
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Original name</TableCell>
-            <TableCell align="center">File name Server</TableCell>
-            <TableCell align="center">created_date</TableCell>
-            <TableCell align="center">start_period</TableCell>
-            <TableCell align="center">end_period</TableCell>
-            <TableCell align="center">size</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {uploadsData.map((file) => (
-            <TableRow
-              key={file.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row" align="center">
-                {file.original_name}
-              </TableCell>
-              <TableCell align="center">{file.filename_server}</TableCell>
-              <TableCell align="center">{file.created_date}</TableCell>
-              <TableCell align="center">{file.start_period}</TableCell>
-              <TableCell align="center">{file.end_period}</TableCell>
-              <TableCell align="center">{file.size} KB</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className="flex items-center justify-center m-8">
+      <table className="table-fixed">
+        <thead className="bg-gray-800 text-gray-300 sticky ">
+          <tr>
+            <th className=" w-48 p-4 rounded-tl-lg">Original name</th>
+            <th className=" w-48 p-4">File name server</th>
+            <th className=" w-48 p-4">Start period</th>
+            <th className=" w-48 p-4">End period</th>
+            <th className=" w-48 p-4">Creation date</th>
+            <th className=" w-48 p-4 rounded-tr-lg">Size</th>
+          </tr>
+        </thead>
+        <tbody className="last-child:bg-green-100 ">
+          {uploadsData &&
+            uploadsData.map((file) => (
+              <tr
+                key={file.id}
+                className="hover:bg-gray-300 hover:text-white hover:font-medium cursor-pointer odd:bg-white even:bg-slate-50"
+              >
+                <td className="text-center p-4 border-2">
+                  {file.original_name}
+                </td>
+                <td className="text-center p-4 border-2">
+                  {file.filename_server}
+                </td>
+                <td className="text-center p-4 border-2">
+                  {file.start_period}
+                </td>
+                <td className="text-center p-4 border-2">{file.end_period}</td>
+                <td className="text-center p-4 border-2">
+                  {file.created_date}
+                </td>
+                <td className="text-center p-4 border-2">{file.size}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
